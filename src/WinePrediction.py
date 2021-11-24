ce449319e0c8c65fa11748684ce13bb8d899d058
Raghuvp01 <78927998+Raghuvp01@users.noreply.github.com>
2021-11-23 21:26:45 -0500

    Update WinePrediction.py

--- src/WinePrediction.py
+++ src/WinePrediction.py
@@ -1,43 +1,96 @@
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
-from pyspark.ml.feature import VectorAssembler
-from pyspark.ml.feature import StandardScaler
-from pyspark.ml.classification import LogisticRegression
-from pyspark.sql.functions import col, count, isnan, when
-from pyspark.mllib.evaluation import MulticlassMetrics
+import sys
+
+import pyspark
+from pyspark.ml import Pipeline
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.feature import IndexToString, StringIndexer, VectorIndexer
+from pyspark.ml.linalg import Vectors
+from pyspark.mllib.linalg import Vectors
+from pyspark.sql.functions import col
+from pyspark.sql.functions import udf
+from pyspark.sql.session import SparkSession
+from pyspark.sql.types import StringType, DoubleType
 
 if __name__ == "__main__":
-    sc = SparkContext(master="local[2]")
-    spark = SparkSession.builder.appName("src Prediction").getOrCreate()
-    data = spark.read.csv("s3://raghuvp/TrainingDataset.csv", header=True, inferSchema=True)
-    data.show(10)
-    data.printSchema()
 
-    new_data = data.select(*(col(c).cast("float").alias(c) for c in data.columns))
-    data.printSchema()
+    # Starting the spark session
+    conf = pyspark.SparkConf().setAppName('winequality')
+    sc = pyspark.SparkContext.getOrCreate()
+    spark = SparkSession(sc)
 
-    data.select([count(when(col(c).isNull(), c)).alias(c) for c in new_data.columns]).show()
-    cols = new_data.columns
-    cols.remove("quality")
+    # Loading the dataset
+    df = spark.read.csv('TrainingDataset.csv', inferSchema='true', header=True, sep=";")
+    df.printSchema()
+    # changing the 'quality' column name to 'label'
+    for col_name in df.columns[1:-1] + ['quality']:
+        df = df.withColumn(col_name, col(col_name).cast('float'))
+    df = df.withColumnRenamed('quality', "label")
 
-    assembler = VectorAssembler(inputCols=cols, outputCol="features")
-    d = assembler.transform(new_data)
-    d.select("features", 'quality').show(truncate=False)
 
-    ss = StandardScaler().setInputCol("features").setOutputCol("Scaled_features")
-    d = ss.fit(d).transform(d)
-    d.select("Scaled_features", 'quality').show()
-    assembled_data = d.select("Scaled_features", "quality")
-    assembled_data.show()
+    # Convert to float format
+    def string_to_float(x):
+        return float(x)
 
-    train, test = d.randomSplit([0.7, 0.3], seed=42)
-    train.show()
-    test.show()
 
-    lr = LogisticRegression(labelCol="quality", featuresCol="Scaled_features", maxIter=40)
-    model = lr.fit(train)
-    pred = model.transform(test)
-    pred.select("Scaled_features", "quality").show(5)
-    metrics = model.summary
-    f_measure = metrics.weightedFMeasure()
-    print(f_measure)
+    # catalog the data
+    def catelogy(r):
+        if 0 <= r <= 6.5:
+            label = "bad"
+        elif 6.5 < r <= 10:
+            label = "good"
+        else:
+            label = "n/a"
+        return label
+
+
+    string_to_float_udf = udf(string_to_float, DoubleType())
+    quality_udf = udf(lambda x: catelogy(x), StringType())
+
+    df = df.withColumn("label", quality_udf("label"))
+
+
+    def transData(data):
+        return data.rdd.map(lambda r: [Vectors.dense(r[:-1]), r[-1]]).toDF(['features', 'label'])
+
+
+    transformed = transData(df)
+
+    labelIndexer = StringIndexer(inputCol='label', outputCol='indexedLabel').fit(transformed)
+
+    featureIndexer = VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(transformed)
+
+    # TODO
+    from pyspark.ml.feature import PCA
+
+    data = transformed
+    pca = PCA(k=6, inputCol="features", outputCol="pcaFeatures")
+    model = pca.fit(data)
+
+    result = model.transform(data).select("pcaFeatures")
+    result.show(truncate=False)
+    (trainingData, testData) = transformed.randomSplit([0.8, 0.2])
+    print("Training Dataset Count: " + str(trainingData.count()))
+    print("Test Dataset Count: " + str(testData.count()))
+
+    # Train a RandomForest model.
+    rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", numTrees=8, maxDepth=20, seed=42)
+
+    # Convert indexed labels back to original labels.
+    labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel", labels=labelIndexer.labels)
+    # Chain indexers and tree in a Pipeline
+    pipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf, labelConverter])
+    # Train model.  This also runs the indexers.
+    model = pipeline.fit(trainingData)
+
+    # Make predictions.
+    predictions = model.transform(testData)
+    # Select example rows to display.
+
+    # Select (prediction, true label) and compute test error
+    evaluator = MulticlassClassificationEvaluator(labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+    accuracy = evaluator.evaluate(predictions)
+    print("accuracy = %g" % accuracy)
+
+    model.save(sys.argv[2])
+    print(sys.argv[2])
